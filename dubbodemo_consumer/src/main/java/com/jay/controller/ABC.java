ccc8fd6d3826395349d44890485af43c2d0f15ae
jay <984687979@qq.com>
2021-11-19 23:43:04 +0800

    新增1111

--- dubbodemo_consumer/src/main/java/com/jay/controller/ABC.java
+++ dubbodemo_consumer/src/main/java/com/jay/controller/ABC.java
@@ -7,5 +7,7 @@ package com.jay.controller;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("1111111111masterw2222");
+        System.out.println("111111111");
+
     }
 }
