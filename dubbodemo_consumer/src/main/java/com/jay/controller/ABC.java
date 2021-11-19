bd118712e0c3234e9bc09ce3faa687819249f5c2
jay <984687979@qq.com>
2021-11-19 23:09:26 +0800

    项目初始化

---
+++ dubbodemo_consumer/src/main/java/com/jay/controller/ABC.java
@@ -0,0 +1,11 @@
+package com.jay.controller;
+
+/**
+ * @author jay
+ * @date 2021/11/19 - 23:08
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+    }
+}
